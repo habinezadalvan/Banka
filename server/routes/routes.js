a06daeb88e09fb99fd9bee3bfd6004bb25464480
habineza leon christian <habinezadalvan@gmail.com>
2019-04-02 21:02:46 +0200

    no change

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import SignUp from '../controllers/signup';
+import auth from '../middleware/auth';
 
 const router = express.Router();
 
-router.post();
+router.post('/api/v1/auth/signup', SignUp.signup);
 
 
 export default router;
