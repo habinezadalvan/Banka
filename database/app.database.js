465736cb7312cebfabf745fffd0bd6b113681539
habineza leon christian <habinezadalvan@gmail.com>
2019-04-23 16:46:24 +0200

    [chore #165328871] this commit creates a swagger documentation

--- database/app.database.js
+++ database/app.database.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
 import router from './routes/routes';
+import swaggerDoc from './swagger.json';
 
 const app = express();
 app.use(bodyParser.json());
+app.use('/swaggerapi', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(router);
 
