46ae108dc54271f5f2c5792f1859ce0de74556d1
habineza leon christian <habinezadalvan@gmail.com>
2019-04-23 18:15:25 +0200

    [chore #165538524] this commit adds a message to heroku

--- database/app.database.js
+++ database/app.database.js
@@ -4,6 +4,9 @@ import router from './routes/routes';
 
 const app = express();
 app.use(bodyParser.json());
+app.get('/', (req, res) => res.status(200).json({
+  message: 'Welcome to banka',
+}));
 
 app.use(router);
 
